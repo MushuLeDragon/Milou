4595d4057b464a4e65c318eacbd374e64a162775
MushuLeDragon <dragon.mushu@hotmail.com>
2017-10-26 14:55:05 +0200

    ajout du marshal

--- src/milou/Milou.java
+++ src/milou/Milou.java
@@ -5,6 +5,13 @@
  */
 package milou;
 
+import generated.Chien;
+import generated.Personne;
+import java.io.FileInputStream;
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
+
 /**
  *
  * @author Mushu
@@ -14,8 +21,17 @@ public class Milou {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         // TODO code application logic here
+
+        JAXBContext jc = JAXBContext.newInstance("generated");
+        Unmarshaller u = jc.createUnmarshaller();
+        Chien mimi = (Chien) u.unmarshal(new FileInputStream("D:\\GoogleDrive\\EPSI.Montpellier.2017\\WebServices\\CodeJava\\Milou\\Files\\milou.xml"));
+        System.out.println("nom du chien :" + mimi.getNom());
+        Personne mait = mimi.getMaitre();
+        System.out.println("nom du maitre :" + mait.getNom());
+        System.out.println("adresse du maitre : " + mait.getAdresse());
+        System.out.println("nombre de puces = " + mimi.getPuces());
     }
-    
+
 }
